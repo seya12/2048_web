7dab6401694954d7945183d78e12284b8dcbecbf
seya12 <lukas.seyr@gmx.at>
2022-07-03 15:32:08 +0200

    first commit

---
+++ src/main/java/spw4/game2048/GameImpl.java
@@ -0,0 +1,47 @@
+package spw4.game2048;
+
+public class GameImpl implements Game {
+    public Game() {
+        // to do ...
+    }
+
+    public int getMoves() {
+        // to do ...
+        return 0;
+    }
+
+    public int getScore() {
+        // to do ...
+        return 0;
+    }
+
+    public int getValueAt(int x, int y) {
+        // to do ...
+        return 0;
+    }
+
+
+    public boolean isOver() {
+        // to do ...
+        return false;
+    }
+
+    public boolean isWon() {
+        // to do ...
+        return false;
+    }
+
+    @Override
+    public String toString() {
+        // to do ...
+        return "";
+    }
+
+    public void initialize() {
+        // to do ...
+    }
+
+    public void move(Direction direction) {
+        // to do ...
+    }
+}
